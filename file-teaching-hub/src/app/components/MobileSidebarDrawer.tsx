a9fb6c0bbb6e9d3b37875cf8c1c7f3c9304a926b
Jeremy-Xxz <jeremy960312@gmail.com>
2026-07-29 12:32:30 +0800

    optimization mobile  version

--- file-teaching-hub/src/app/components/MobileSidebarDrawer.tsx
+++ file-teaching-hub/src/app/components/MobileSidebarDrawer.tsx
@@ -33,7 +33,7 @@ export default function MobileSidebarDrawer({ children }: MobileSidebarDrawerPro
       {isOpen && (
         <div className="fixed inset-0 z-50 flex">
           <div
-            className="absolute inset-0 bg-black/20 backdrop-blur-xs"
+            className="absolute inset-0 bg-black/10 backdrop-blur-xs"
             onClick={() => setIsOpen(false)}
           />
 
