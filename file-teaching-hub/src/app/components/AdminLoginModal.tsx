b594d1f85e342fd8030acb04f05e0809b3778097
Jeremy-Xxz <jeremy960312@gmail.com>
2026-08-06 15:54:13 +0800

    password

--- file-teaching-hub/src/app/components/AdminLoginModal.tsx
+++ file-teaching-hub/src/app/components/AdminLoginModal.tsx
@@ -8,7 +8,7 @@ export default function AdminLoginModal({ onClose, onLoginSuccess }: { onClose: 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
     // 預設簡易密碼驗證，你可以自由修改
-    if (password === 'podcast1234') {
+    if (password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD) {
       onLoginSuccess();
     } else {
       alert('密碼輸入錯誤，請再試一次！');
